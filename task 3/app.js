750645125d0b1daea622137eaeca080ff0b6d576
AllyintheCode <allyasadzade@gmail.com>
2025-02-20 23:02:04 +0400

    task 3 added

---
+++ task 3/app.js
@@ -0,0 +1,28 @@
+let number = +prompt("Enter the number")
+if (number === 1) {
+  console.log("This month January");
+} else if (number === 2) {
+  console.log("This Month February");
+} else if (number === 3) {
+  console.log("This Month March");
+} else if (number === 4) {
+  console.log("This Month April");
+} else if (number === 5) {
+  console.log("This Month May");
+} else if (number === 6) {
+  console.log("This Month June");
+} else if (number === 7) {
+  console.log("This Month July");
+} else if (number === 8) {
+  console.log("This Month August");
+} else if (number === 9) {
+  console.log("This Month September");
+} else if (number === 10) {
+  console.log("This Month October");
+} else if (number === 11) {
+  console.log("This Month November");
+} else if (number === 12) {
+  console.log("This Month December");
+} else {
+  console.log("Enter correct information");
+}
