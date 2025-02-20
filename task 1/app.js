ef77ab70e382e1cc9717a332d29774a416acabb1
AllyintheCode <allyasadzade@gmail.com>
2025-02-20 22:52:02 +0400

    task1 and task 2 added

---
+++ task 1/app.js
@@ -0,0 +1,7 @@
+let weight = +prompt("Murad cekin ne qederdir?")
+let experince = +prompt("tecruben ne qederdir murad?")
+if (weight === 70 && experince >= 5) {
+  console.log("Murad polnu yarisa gire bilersen");
+} else {
+  console.log("Gedemmirsen teessufki");
+}
---
+++ task 2/app.js
@@ -0,0 +1,18 @@
+let number = +prompt("Enter the number");
+if (number === 1) {
+  console.log("Today is Monday");
+} else if (number === 2) {
+  console.log("Today is Tuesday");
+} else if (number === 3) {
+  console.log("Today is Wednesday");
+} else if (number === 4) {
+  console.log("Today is Thursday");
+} else if (number === 5) {
+  console.log("Today is Friday");
+} else if (number === 6) {
+  console.log("Today is Saturday");
+} else if (number === 7) {
+  console.log("Today is Sunday");
+} else {
+  console.log("Be smart");
+}
