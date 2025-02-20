3c175d2274e799bee642e4572981216ed5ea6695
AllyintheCode <allyasadzade@gmail.com>
2025-02-20 23:58:42 +0400

    task 7 added

---
+++ task 7/app.js
@@ -0,0 +1,12 @@
+let direction = prompt("Enter direction")
+if (direction == "up") {
+  console.log("Go up");
+} else if (direction == "right") {
+  console.log("Go right");
+} else if (direction == "left") {
+  console.log("Go left");
+} else if (direction == "down") {
+  console.log("Go down");
+} else {
+  console.log("Don't go");
+}
